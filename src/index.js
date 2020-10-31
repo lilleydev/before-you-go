9c4a690195eba22e1cba54817f2224d266cd9c29
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-30 20:01:41 -0700

    index setup with basic app import and render to DOM

--- src/index.js
+++ src/index.js
@@ -4,3 +4,17 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { compostWithDevTools } from "redux-devtools-extension";
+
+import App from "./components/App";
+
+const store = createStore(
+  rootReducer,
+  compostWithDevTools(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);

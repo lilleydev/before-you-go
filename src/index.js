7067a3980eb659fc1bd234dbcd708a16187313a4
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-30 19:59:41 -0700

    set up inital imports

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { compostWithDevTools } from "redux-devtools-extension";

d9f6df8d07fc604d430a13b44bebdc59845f3ad5
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-30 20:12:48 -0700

    import rootReducer

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { compostWithDevTools } from "redux-devtools-extension";
-
+import rootReducer from "./reducers/rootReducer";
 import App from "./components/App";
 
 const store = createStore(

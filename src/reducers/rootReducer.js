64801617ffeb260d98e486d7523a862bc0029be3
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-03 12:23:09 -0800

    add beerReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { coursesReducer } from "./coursesReducer";
 import { usersReducer } from "./usersReducer";
+import { beerReducer } from "./beerReducer";
 
 export const rootReducer = combineReducers({
   users: usersReducer,
   courses: coursesReducer,
+  beers: beerReducer,
 });

b8b48da2eae3d22738666b92011ecb035237d20a
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-03 08:17:04 -0800

    inital setup class component with hello verify message

---
+++ src/components/CoursesContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CoursesContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello From Courses Container</h3>
+      </div>
+    );
+  }
+}

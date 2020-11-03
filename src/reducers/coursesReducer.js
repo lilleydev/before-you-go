27387ca4879cc1b3f8c884ab5f1672054d281f24
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-03 15:34:08 -0800

    add case ADD COURSE

--- src/reducers/coursesReducer.js
+++ src/reducers/coursesReducer.js
@@ -2,6 +2,8 @@ export const coursesReducer = (state = [], action) => {
   switch (action.type) {
     case "FETCH_COURSES":
       return action.payload;
+    case "ADD_COURSE":
+      return [...state, action.payload];
     default:
       return state;
   }

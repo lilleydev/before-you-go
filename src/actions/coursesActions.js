ca03eeb288ba3f33bae202df7b75d549c3fb7aba
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-03 15:32:23 -0800

    add addCourse action

--- src/actions/coursesActions.js
+++ src/actions/coursesActions.js
@@ -5,3 +5,15 @@ export const fetchCourses = () => {
       .then((courses) => dispatch({ type: "FETCH_COURSES", payload: courses }));
   };
 };
+
+export const addCourse = (course) => {
+  return (dispatch) => {
+    fetch("http://localhost:3000/courses", {
+      method: "POST"
+      body: JSON.stringify(course),
+      headers: { 'Content-Type': 'application/json'}
+    })
+      .then((resp) => resp.json())
+      .then((course) => dispatch({ type: "ADD_COURSE", payload: courses }));
+  };
+};

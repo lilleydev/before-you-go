f2b50aac97851119db2c5e73119f09b30824de5d
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-03 14:48:43 -0800

    set up class component with initial state

---
+++ src/components/CoursesForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class CoursesForm extends Component {
+  state = {
+    name: "",
+    street: "",
+    city: "",
+    state: "",
+    difficulty: "",
+    mixed_use_park: false,
+  };
+  render() {
+    return <div></div>;
+  }
+}

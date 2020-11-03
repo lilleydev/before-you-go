903c906dece2d4998d0db22253901c63f87c7637
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-03 08:24:51 -0800

    combine users and courses reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
-import coursesReducer from "./coursesReducer";
-import usersReducer, { usersReducer } from "./usersReducers";
+import { coursesReducer } from "./coursesReducer";
+import { usersReducer } from "./usersReducer";
 
 export const rootReducer = combineReducers({
   users: usersReducer,

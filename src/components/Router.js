972cb3a04196bbaac65af12c16af30cfa92e5409
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-03 16:13:35 -0800

    add about and home

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-
+import Home from "../components/Home";
+import CoursesContainer from "./CoursesContainer";
+import About from "../components/About";
 const Router = () => {
   return <div>hey from router</div>;
 };

214600b624de232f31284a9446f299a4948cc054
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-03 18:19:47 -0800

    add home about courses routes

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,8 +3,15 @@ import { Switch, Route } from "react-router-dom";
 import Home from "../components/Home";
 import CoursesContainer from "./CoursesContainer";
 import About from "../components/About";
+
 const Router = () => {
-  return <div>hey from router</div>;
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/courses" component={CoursesContainer} />
+    </Switch>
+  );
 };
 
 export default Router;

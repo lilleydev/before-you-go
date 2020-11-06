1e5c1148542cbce2656b0999c35a3e731af1d271
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-06 11:52:36 -0800

    add currentUserReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import { coursesReducer } from "./coursesReducer";
 import { usersReducer } from "./usersReducer";
 import { beerReducer } from "./beerReducer";
+import { currentUserReducer } from "./currentUserReducer";
 
 export const rootReducer = combineReducers({
   users: usersReducer,
   courses: coursesReducer,
   beers: beerReducer,
+  currentUser: currentUserReducer,
 });

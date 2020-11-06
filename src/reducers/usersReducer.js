cb2e430dca4a2df495aeeb4536e92bef1fdc9935
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-06 13:47:08 -0800

    fix typo in reducer

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,7 +1,7 @@
 export const usersReducer = (state = [], action) => {
   switch (action.type) {
     case "FETCH_USERS":
-      return action.paylod;
+      return action.payload;
     default:
       return state;
   }

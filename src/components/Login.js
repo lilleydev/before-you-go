90152469534b56601ccc27fa32850fb4fa546151
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-06 13:46:20 -0800

    build login form

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <form onSubmit={}>
+      <input type="text" name="username" onChange={} value={} />
+      <input type="text" name="password" onChange={} value={} />
+      <input type="submit" value="Log In" />
+    </form>
+  );
+};
+
+export default Login;

811d3f799c9b511307a4bdb46d01ba611b77ab01
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-06 18:35:39 -0800

    convert to class component

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,17 @@ import Router from "./Router";
 import CoursesContainer from "./CoursesContainer";
 import Login from "./Login";
 
-const App = () => {
-  return (
-    <div>
-      Hello World From App Class
-      <CoursesContainer />,
-      <Login />,
-      <Router />
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello World From App Class
+        <CoursesContainer />,
+        <Login />,
+        <Router />
+      </div>
+    );
+  }
+}
 
 export default App;

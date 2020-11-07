5e44fa3a71d423c493ec6cd14cab74ebfa2cf7be
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-07 14:55:14 -0800

    add clear current user case

--- src/reducers/currentUserReducer.js
+++ src/reducers/currentUserReducer.js
@@ -2,6 +2,8 @@ export const currentUserReducer = (state = null, action) => {
   switch (action.type) {
     case "SET_CURRENT_USER":
       return action.user;
+    case "CLEAR_CURRENT_USER":
+      return null;
     default:
       return state;
   }

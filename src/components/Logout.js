d30afcd6648d9f8dc521288f9b05a2cbd25ed138
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-07 14:54:15 -0800

    add logout component

---
+++ src/components/Logout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { connect } from "react-redux";
+import { logout } from "../actions/currentUser";
+
+const Logout = ({ logout }) => {
+  return (
+    <form onSubmit={logout}>
+      <input type="submit" value="Logout" />
+    </form>
+  );
+};
+
+export default connect(null, { logout })(Logout);

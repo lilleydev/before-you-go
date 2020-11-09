15d8a00dae5a857fc15eb867c36bc13e590ade44
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-08 20:47:41 -0800

    fix getMyBucketLists resp typo for if resp.erro

--- src/actions/bucketLists.js
+++ src/actions/bucketLists.js
@@ -1,7 +1,28 @@
 //sync
-export const setBucketLists = (bucketLists) => {
+export const setBucketLists = (bucket_lists) => {
   return {
     type: "SET_BUCKET_LISTS",
-    bucketLists,
+    bucket_lists,
+  };
+};
+
+export const getMyBucketLists = () => {
+  return (dispatch) => {
+    return fetch("http://localhost:3000/api/v1/bucket_lists", {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((resp) => {
+        if (resp.error) {
+          alert(resp.error);
+        } else {
+          dispatch(setBucketLists(resp.data));
+        }
+      })
+      .catch(console.log);
   };
 };

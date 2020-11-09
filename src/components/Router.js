a458a1d2037305873bad46c9d14f6c0587818817
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-08 20:46:55 -0800

    add paths signup

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,12 +3,19 @@ import { Switch, Route } from "react-router-dom";
 import Home from "../components/Home";
 import CoursesList from "./CoursesList";
 import About from "../components/About";
+import Login from "../components/Login";
+import Logout from "../components/Logout";
+import Signup from "./Signup";
 
 const Router = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/logout" component={Logout} />
       <Route path="/about" component={About} />
+      <Route path="/signup" component={Signup} />
+
       <Route path="/courses" component={CoursesList} />
     </Switch>
   );

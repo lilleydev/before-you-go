0f9933a83d0d9bd3d4cea67891de604c1f16bd55
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-11 15:09:21 -0800

    add routes for lists and logic to render full navbar to currentUser

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,32 +2,41 @@ import React from "react";
 // import Login from "./Login";
 // import Logout from "./Logout";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavBar = ({ currentUser }) => {
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         {currentUser ? <p>What's up {currentUser.attributes.name}!!</p> : ""}
-        {/* {currentUser ? <Logout /> : <Login />} */}
-        <Link to="/" className="brand-logo">
-          Home
-        </Link>
-        <Link to="/about" className="brand-logo">
-          About
-        </Link>
-        <Link to="/courses" className="brand-logo">
-          Courses
-        </Link>
-        <Link to="/login" className="brand-logo">
-          Login
-        </Link>
-        <Link to="/logout" className="brand-logo">
-          Logout
-        </Link>
-        <Link to="/signup" className="brand-logo">
-          Sign Up
-        </Link>
+        {currentUser ? (
+          <>
+            <NavLink exact activeClass to="/logout" className="brand-logo">
+              Logout |
+            </NavLink>
+            <NavLink exact activeClass to="/" className="brand-logo">
+              Home |
+            </NavLink>
+
+            <NavLink exact activeClass to="/about" className="brand-logo">
+              About |
+            </NavLink>
+            <NavLink exact activeClass to="/bucketlist" className="brand-logo">
+              Gotta Do! |
+            </NavLink>
+            <NavLink
+              exact
+              activeClass
+              to="/bucketlist/new"
+              className="brand-logo"
+            >
+              Add to My List |
+            </NavLink>
+            <NavLink exact activeClass to="/courses" className="brand-logo">
+              Courses |
+            </NavLink>
+          </>
+        ) : null}
       </div>
     </nav>
   );

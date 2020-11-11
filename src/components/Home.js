6de97b5974d53a0994bc163eb751397813710e2d
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-11 15:06:08 -0800

    add signup and login links

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Welcome to Before You Go!</div>;
+  return (
+    <div>
+      <h1> Welcome to Before You Go!</h1>
+      <h2>
+        <Link to="/signup" className="brand-logo">
+          Sign Up
+        </Link>
+        or
+        <Link to="/login" className="brand-logo">
+          Login
+        </Link>
+      </h2>
+    </div>
+  );
 };
 
 export default Home;

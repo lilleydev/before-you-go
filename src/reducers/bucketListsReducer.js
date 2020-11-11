1773c57f3fcb25872cb78dcc0fc94d8c956945eb
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-11 15:11:08 -0800

    add clear lists

--- src/reducers/bucketListsReducer.js
+++ src/reducers/bucketListsReducer.js
@@ -2,6 +2,8 @@ export const bucketLists = (state = [], action) => {
   switch (action.type) {
     case "SET_BUCKET_LISTS":
       return action.bucket_lists;
+    case "CLEAR_LISTS":
+      return [];
     default:
       return state;
   }

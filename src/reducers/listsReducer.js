ed976ca76fd52beb580c441a5a4b144053be5696
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-14 08:12:18 -0800

    fix typo for state.concat add_list

--- src/reducers/listsReducer.js
+++ src/reducers/listsReducer.js
@@ -2,6 +2,8 @@ export const lists = (state = [], action) => {
   switch (action.type) {
     case "SET_LISTS":
       return action.lists;
+    case "ADD_LIST":
+      return state.concat(action.list);
     case "CLEAR_LISTS":
       return [];
     default:

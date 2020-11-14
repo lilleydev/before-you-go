b9676ffafa480cc8aba71171b11546c935db2cda
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-14 10:34:48 -0800

    update to render all attributes

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -1,15 +1,17 @@
 import React from "react";
 
-const ListCard = ({ lists }) => {
-  return lists ? (
+const ListCard = ({ list }) => {
+  return list ? (
     <div>
-      <h3>{lists.attributes.name}</h3>
-      <h4>{lists.attributes.description}</h4>
+      <h3>{list.attributes.name}</h3>
+      <h4>{list.attributes.description}</h4>
+      <h4>{list.attributes.start_date}</h4>
+      <h4>{list.attributes.end_date}</h4>
 
       {/* <Link to={`/_list_cards/${_list_card.id}/edit`}>Edit this _list_card</Link> */}
     </div>
   ) : (
-    <p>This the the _list_card card with no lists</p>
+    <p>This the the _list_card card with no list</p>
   );
 };
 

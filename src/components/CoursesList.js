da71a6d98fd00584e63b1632b5553eed45fc7285
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-16 15:12:12 -0800

    add styling divs and labels

--- src/components/CoursesList.js
+++ src/components/CoursesList.js
@@ -5,41 +5,35 @@ const CoursesList = ({ courses, deleteCourse }) => {
   // const courseInfo = courses.data;
   // debugger;
   return (
-    <div id="main">
-      <div class="inner">
-        <section>
-          <header class="major">
-            <h2>Courses in this locale</h2>
-          </header>
-          <div class="posts">
-            {courses ? (
-              <>
-                {courses.map((course) => (
-                  <article>
-                    <li key={course.id}>
-                      <h2>{course.attributes.name}</h2>
-                      <h3>{course.attributes.difficulty}</h3>
-                      <p>
-                        {course.attributes.street} - {course.attributes.city} -{" "}
-                        {course.attributes.state}
-                      </p>
-                      <button onClick={() => deleteCourse(course)}>
-                        Delete
-                      </button>
-                      {/* <button onClick={() => addCourseToList(course)}>
+    <section>
+      <header class="major">
+        <h2>Courses in this locale</h2>
+      </header>
+      <div class="posts">
+        {courses ? (
+          <>
+            {courses.map((course) => (
+              <article>
+                <li key={course.id}>
+                  <h2>{course.attributes.name}</h2>
+                  <h3>{course.attributes.difficulty}</h3>
+                  <p>
+                    {course.attributes.street} - {course.attributes.city} -{" "}
+                    {course.attributes.state}
+                  </p>
+                  <button onClick={() => deleteCourse(course)}>Delete</button>
+                  {/* <button onClick={() => addCourseToList(course)}>
                         Play This!
                       </button> */}
-                    </li>
-                  </article>
-                ))}
-              </>
-            ) : (
-              "there are no courses"
-            )}
-          </div>
-        </section>
+                </li>
+              </article>
+            ))}
+          </>
+        ) : (
+          "there are no courses"
+        )}
       </div>
-    </div>
+    </section>
   );
 };
 

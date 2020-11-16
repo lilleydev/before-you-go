e84b54fe4abb3ba1e9545f8f6fb33d38aab0c9c8
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-16 15:11:18 -0800

    add welcome paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,11 @@ import React from "react";
 
 const About = () => {
   return (
-    <div id="main">
-      <h2>
-        This application was designed with a traveling spirit in mind! <br />
-        Add a new location to your travel plans, include your travel dates{" "}
-        <br />
-        and then check out courses to add them to your list!
-      </h2>
-    </div>
+    <h2>
+      This application was designed with a traveling spirit in mind! <br />
+      Add a new location to your travel plans, include your travel dates <br />
+      and then check out courses to add them to your list!
+    </h2>
   );
 };
 

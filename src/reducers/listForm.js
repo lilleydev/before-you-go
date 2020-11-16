ae8d2538e1a10858a6b7ed69dc8ac234998ba2bd
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-16 15:15:10 -0800

    add set_form_data_for_edit

--- src/reducers/listForm.js
+++ src/reducers/listForm.js
@@ -1,14 +1,22 @@
-const initalState = { name: "", description: "", stateDate: "", endDate: "" };
+const initialState = {
+  name: "",
+  description: "",
+  startDate: "",
+  endDate: "",
+};
 
-export const listForm = (state = initalState, action) => {
+export const listForm = (state = initialState, action) => {
   switch (action.type) {
     case "UPDATE_LIST_FORM":
-      return {
+      const returnVal = {
         ...state,
         [action.formData.name]: action.formData.value,
       };
+      return returnVal;
     case "RESET_LIST_FORM":
-      return initalState;
+      return initialState;
+    case "SET_FORM_DATA_FOR_EDIT":
+      return action.listFormData;
     default:
       return state;
   }

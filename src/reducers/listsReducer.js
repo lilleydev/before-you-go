7a2aac427bf9cd6f90c9b111184e35183d4b35b8
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-16 15:15:34 -0800

    add update_list

--- src/reducers/listsReducer.js
+++ src/reducers/listsReducer.js
@@ -1,11 +1,17 @@
-export const lists = (state = [], action) => {
+const initialState = [];
+
+export const lists = (state = initialState, action) => {
   switch (action.type) {
     case "SET_LISTS":
       return action.lists;
     case "ADD_LIST":
       return state.concat(action.list);
+    case "UPDATE_LIST":
+      return state.map((list) =>
+        list.id === action.list.id ? action.list : list
+      );
     case "CLEAR_LISTS":
-      return [];
+      return initialState;
     default:
       return state;
   }

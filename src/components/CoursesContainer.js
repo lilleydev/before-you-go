de3925b5efbc4c8e6b6d182ba6e4edc8c7250aa2
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-16 20:18:28 -0800

    remove form to separate component

--- src/components/CoursesContainer.js
+++ src/components/CoursesContainer.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchCourses } from "../actions/coursesActions";
 import { fetchBreweries } from "../actions/beerAction";
-import CoursesForm from "./CoursesForm";
 import CoursesList from "./CoursesList";
 
 class CoursesContainer extends Component {
@@ -13,9 +12,7 @@ class CoursesContainer extends Component {
   render() {
     return (
       <div>
-        <h3>Hello From Courses Container</h3>
         <CoursesList />
-        <CoursesForm />
       </div>
     );
   }

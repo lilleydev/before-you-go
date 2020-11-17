44f95279e5b8483dccf0edd3d6886733ac7c2240
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-16 20:20:16 -0800

    add add_course and set_courses

--- src/reducers/coursesReducer.js
+++ src/reducers/coursesReducer.js
@@ -2,8 +2,10 @@ export const coursesReducer = (state = [], action) => {
   switch (action.type) {
     case "FETCH_COURSES":
       return action.payload;
+    case "SET_COURSES":
+      return action.courses;
     case "ADD_COURSE":
-      return [...state, action.payload];
+      return state.concat(action.course);
     case "DELETE_COURSE":
       // debugger;
       return state.filter((course) => course.id !== action.payload.id);
